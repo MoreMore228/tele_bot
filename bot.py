5fca8237ad1f8e9c19fbe4c2ed209730f87304bc
Yarik <moritka@inbox.ru>
2022-10-24 16:04:01 +0500

    Пытаюсь ссделать приветствие

--- bot.py
+++ bot.py
@@ -8,11 +8,14 @@ rate_of_dollar_val = PrintDollar()
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(content_types=['text'])
 def start(message):
-    welcome_sticker = open('stikers/welcome.webp', 'rb')
-    bot.send_sticker(message.chat.id, welcome_sticker)
-    bot.send_message(message.chat.id, "Привет, {0.first_name}".format(message.from_user))
+    if message.chat.type == 'Привет, бот!':
+        welcome_sticker = open('stikers/welcome.webp', 'rb')
+        bot.send_sticker(message.chat.id, welcome_sticker)
+        bot.send_message(message.chat.id, "Привет, {0.first_name}".format(message.from_user))
+
+        
 
 @bot.message_handler(commands=["help"])
 def help(message):
