824b834cc2e5eb1accba0e59665d7f00d89b9da4
Yarik <moritka@inbox.ru>
2022-10-24 16:35:52 +0500

    Ответ на некоторые слова и команды

--- bot.py
+++ bot.py
@@ -7,16 +7,6 @@ rate_of_dollar_val = PrintDollar()
 
 
 bot = telebot.TeleBot(config.TOKEN)
-
-@bot.message_handler(content_types=['text'])
-def start(message):
-    if message.chat.type == 'Привет, бот!':
-        welcome_sticker = open('stikers/welcome.webp', 'rb')
-        bot.send_sticker(message.chat.id, welcome_sticker)
-        bot.send_message(message.chat.id, "Привет, {0.first_name}".format(message.from_user))
-
-        
-
 @bot.message_handler(commands=["help"])
 def help(message):
     bot.send_message(message.chat.id, "/help - все команды бота\n/rateOFdollar - курс доллара")
@@ -25,14 +15,17 @@ def help(message):
 def rate_of_dollar(message):
     bot.send_message(message.chat.id, 'Курс - {0}'.format(rate_of_dollar_val))
 
-bot.polling(none_stop=True)
+@bot.message_handler(content_types = ['text'])
+def answer(message):
+    if message.text.lower() == 'привет':
+        welcome_sticker = open('stikers/welcome.webp', 'rb')
+        bot.send_sticker(message.chat.id, welcome_sticker)
+        bot.send_message(message.chat.id, "Привет, {0.first_name}".format(message.from_user))
+    else:
+        # bot.send_message(message.chat.id, 'Не знаю что сказать :(')
+        None
 
-
-
-
-
-
-
+bot.polling(none_stop=True, interval=0)
 
 
 
