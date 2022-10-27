a5e3a3b9cb5993b49a28dfa5fb52c093be589ccc
Михаил2403 <mihailpickalev@gmail.com>
2022-10-27 17:09:17 +0500

    button

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot
 import config
+import keyboard_funks as kb
 from cur_pars import CurrencyParsing
 
 # Список мата, для того, чтобы ругать людей :)
@@ -7,6 +8,7 @@ swearing = ['бля', 'блять', 'хуй', 'нахуй']
 
 # Бот и его ТОКЕН
 bot = telebot.TeleBot(config.TOKEN)
+
 
 # ФУНКЦИЯ HELP
 @bot.message_handler(commands=["help"])
@@ -17,33 +19,36 @@ def help(message):
 # ФУНКЦИЯ START
 @bot.message_handler(commands=["start"])
 def help(message):
-    bot.send_message(message.chat.id, "Привет, я бот!\nЯ могу сказать тебе привет (команда: 'Привет')\nЯ могу подсказать все остальные команды (команда: /help)\nА еще могу отругать за 4 матерных слова!!!!")
+    bot.send_message(message.chat.id, "Привет, я бот!\nЯ могу сказать тебе привет (команда: 'Привет')\nЯ могу подсказать все остальные команды (команда: /help)\nВыведу курс некоторых валют\nА еще могу отругать за 4 матерных слова!!!!", reply_markup=kb.keyboard_start())
 
 
 # ФУНКЦИЯ КУРСА ВАЛЮТ
 @bot.message_handler(commands=['exchange'])
 def rate_of_dollar(message):
-    bot.send_message(message.chat.id, 'Выберете курс, который хотите увидеть:\n/USD\n/EUR\n/CNY\n/GBP')
+    bot.send_message(message.chat.id, 'Выберете курс, который хотите увидеть:\n/USD\n/EUR\n/CNY\n/GBP', reply_markup=kb.keyboard_exchange())
+    
 
 # для доллара 
 @bot.message_handler(commands=['USD'])
 def answer(message):
-    bot.send_message(message.chat.id, 'Курс Доллара: {}'.format((CurrencyParsing('USD').get_val())))
+    bot.send_message(message.chat.id, 'Курс Доллара: {}'.format((CurrencyParsing('USD').get_val())), reply_markup=kb.after_rate())
+    
 
 # для евро
 @bot.message_handler(commands=['EUR'])
 def answer(message):
-    bot.send_message(message.chat.id, 'Курс Евро: {}'.format((CurrencyParsing('EUR').get_val())))
+    bot.send_message(message.chat.id, 'Курс Евро: {}'.format((CurrencyParsing('EUR').get_val())), reply_markup=kb.after_rate())
+
 
 # для юаня
 @bot.message_handler(commands=['CNY'])
 def answer(message):
-    bot.send_message(message.chat.id, 'Курс Юаня: {}'.format((CurrencyParsing('CNY').get_val())))
+    bot.send_message(message.chat.id, 'Курс Юаня: {}'.format((CurrencyParsing('CNY').get_val())), reply_markup=kb.after_rate())
 
 # для стерлингов
 @bot.message_handler(commands=['GBP'])
 def answer(message):
-    bot.send_message(message.chat.id, 'Курс Стерлингов: {}'.format((CurrencyParsing('GBP').get_val())))
+    bot.send_message(message.chat.id, 'Курс Стерлингов: {}'.format((CurrencyParsing('GBP').get_val())), reply_markup=kb.after_rate())
 
 # обработка текста
 @bot.message_handler(content_types = ['text'])
@@ -53,8 +58,13 @@ def answer(message):
         bot.send_sticker(message.chat.id, welcome_sticker)
         bot.send_message(message.chat.id, "Привет, {0.first_name}".format(message.from_user))
 
+    if message.text.lower() == "спасибо":
+        pls_sticker = open("stikers/pls.webp", 'rb')
+        bot.send_sticker(message.chat.id, pls_sticker)
+        bot.send_message(message.chat.id, "Всегда пожалуйста, еба", reply_markup=kb.keyboard_default())
     if message.text.lower() in swearing:
-        bot.send_message(message.chat.id, 'Вая, не ругайся!!!')
+        fuck_sticker = open("stikers/fuck.webp", 'rb')        
+        bot.send_sticker(message.chat.id, fuck_sticker)
     
     else:
         None
---
+++ keyboard_funks.py
@@ -0,0 +1,39 @@
+from telebot import types
+
+
+
+def keyboard_default():
+    keyboard_default = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("/help")
+    btn2 = types.KeyboardButton("/exchange")
+    keyboard_default.add(btn1, btn2)
+    return keyboard_default
+
+
+def keyboard_start():
+    keyboard_for_start = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Привет")
+    btn2 = types.KeyboardButton("/help")
+    btn3 = types.KeyboardButton("/exchange")
+    keyboard_for_start.add(btn1, btn2, btn3)
+    return keyboard_for_start
+
+
+def keyboard_exchange():
+    keyboard_for_rate = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("/USD")
+    btn2 = types.KeyboardButton("/EUR")
+    btn3 = types.KeyboardButton("/CNY")
+    btn4 = types.KeyboardButton("/GBP")
+    btn5 = types.KeyboardButton("/help")
+    keyboard_for_rate.add(btn1, btn2, btn3, btn4, btn5)
+    return keyboard_for_rate
+
+
+def after_rate():
+    keyboard_after_rate = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Спасибо")
+    btn2 = types.KeyboardButton("/exchange")
+    btn3 = types.KeyboardButton("/help")
+    keyboard_after_rate.add(btn1, btn2, btn3)
+    return keyboard_after_rate
